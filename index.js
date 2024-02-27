0159c8e6ac2f987f8827778c324688629f8a82db
coderrehad <engr.rehad@gmail.com>
2024-02-27 19:46:07 +0600

    ES6 Module Export Import

---
+++ index.js
@@ -0,0 +1,7 @@
+import Device from "./shop.js";
+
+let obj = new Device()
+
+console.log(obj.Apple)
+console.log(obj.Lenevo)
+console.log(obj.HP)
---
+++ shop.js
@@ -0,0 +1,7 @@
+class Laptop{
+    Lenevo = "ThinkPad"
+    HP = "AB12"
+    Apple = "AB12"
+}
+
+export default Laptop;
